4af57db6994b2044c9462dec769bb937c8a27ee6
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2020-10-06 17:14:37 +0900

    Update 1

---
+++ inc/footer.js
@@ -0,0 +1,14 @@
+str = ''
+str += '<div id="header">'
+str += ''
+str += '	<div class="util">'
+str += '		<ul>'
+str += '			<li><a href="index.html">Home</a><span class="bar"></span></li>'
+str += '			<li><a href="sitemap.html">사이트맵</a><span class="bar"></span></li>'
+str += '			<li><a href="login.html">직원전용</a></li>'
+str += '		</ul>'
+str += '	</div>'
+str += ''
+str += '</div>'
+
+document.write(str);
---
+++ inc/header.js
@@ -0,0 +1,15 @@
+str = ''
+str += '<div id="header">'
+str += ''
+str += '	<ul class="tab">'
+str += '		<li><a href="index.html">LMS 발송</a></li>'
+str += '		<li class="on"><a href="02_list.html">인력 관리</a></li>'
+str += '		<li><a href="03.html">발송 이력</a></li>'
+str += '		<li><a href="04.html">관리자 계정</a></li>'
+str += '	</ul>'
+str += ''
+str += '	<a href="login.html" class="btn_sml log"><span>로그아웃</span></a>'
+str += ''
+str += '</div>'
+
+document.write(str);
