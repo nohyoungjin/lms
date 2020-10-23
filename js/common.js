2a765de51132e0c89c12191c6f2604a7882b6192
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2020-10-23 16:01:09 +0900

    Update 6

--- js/common.js
+++ js/common.js
@@ -264,7 +264,7 @@ function sample6_execDaumPostcode() {
 			var addr = ''; // 주소 변수
 			var extraAddr = ''; // 참고항목 변수
 
-			//사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+			// 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
 			if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
 				addr = data.roadAddress;
 			} else { // 사용자가 지번 주소를 선택했을 경우(J)
